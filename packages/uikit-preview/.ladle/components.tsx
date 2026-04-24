8a04b18f49c368dd023f86c50b517596d8813356
Rohit Sharma <rohit.sharma@archonlabs.com>
2026-04-24 15:03:13 +0200

    Fix story provider.

--- packages/uikit-preview/.ladle/components.tsx
+++ packages/uikit-preview/.ladle/components.tsx
@@ -2,10 +2,4 @@ import type { ReactNode } from 'react';
 
 import '../styled-system/styles.css';
 
-type ProviderProps = {
-  children: ReactNode;
-};
-
-export function Provider({ children }: ProviderProps) {
-  return children;
-}
+export const Provider = ({ children }: { children: ReactNode }) => children;
